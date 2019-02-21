0da6abd2b8f7efb35a45b7d41c13922f7b6bda4c
Christian Zehetner <mail@christianzehetner.at>
2019-02-21 16:53:01 +0100

    Exclude docs page from public views.

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ namespace mgfagency\documentation;
 use Kirby;
 
 \Kirby::plugin('mgfagency/documentation', [
+  'routes' => function ($kirby) {
+    return [
+      [
+        'pattern' => $kirby->option('mgfagency.documentation.contentfolder', 'documentation'),
+        'action' => function () {
+          return false;
+        }
+      ]
+    ];
+  },
   'api' => include __DIR__ . '/includes/api.php',
   'templates' => [
     'doc' => __DIR__ . '/templates/doc.php',

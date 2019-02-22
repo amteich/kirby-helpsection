99d7f8213638a6f863b0c04beccfc36d2b85b078
Christian Zehetner <mail@christianzehetner.at>
2019-02-22 08:24:35 +0100

    Added editorconfig.

--- includes/api.php
+++ includes/api.php
@@ -1,61 +1,61 @@
 <?php
 
 return [
-  'routes' => [
+    'routes' => [
     [
-      // Get the first set of pages to display primary nav
-      'pattern' => 'mgfagency/helpsection/index',
-      'action'  => function () {
+        // Get the first set of pages to display primary nav
+        'pattern' => 'mgfagency/helpsection/index',
+        'action'  => function () {
         $slug = option('mgfagency.helpsection.contentfolder', 'helpsection');
         $pages = [];
         $page = page($slug);
 
         if ($page == null) {
-          return [
+            return [
             'status' => 'failed',
             'error' => "Please create a new content-folder <code>$slug</code> to show it here.",
-          ];
+            ];
         }
 
         foreach (page($slug)->children() as $page) {
-          $pagedata = [
+            $pagedata = [
             'title' => (string)$page->title(),
             'id' => (string)$page->id(),
-          ];
-    
-          if ($page->hasVisibleChildren()) {
+            ];
+
+            if ($page->hasVisibleChildren()) {
             $children = $page->children()->visible();
             $childrendata = [];
             foreach ($children as $child) {
-              $childrendata[] = [
+                $childrendata[] = [
                 'title' => (string)$child->title(),
                 'id' => (string)$child->id(),
                 'slug' => (string)$child->id(),
                 'hasChildren' => (bool)$child->hasChildren(),
                 'rendered' => (string)$child->render(),
-              ];
+                ];
             }
             $pagedata['children'] = $childrendata;
-          }
-          
-          $pages[] = $pagedata;
+            }
+
+            $pages[] = $pagedata;
         }
-    
+
         return [
-          'slug' => $slug,
-          'pages' => $pages,
-          'status' => 'ok',
+            'slug' => $slug,
+            'pages' => $pages,
+            'status' => 'ok',
         ];
-      },
+        },
     ],
     [
-      // Get the first set of pages to display primary nav
-      'pattern' => 'mgfagency/helpsection/page/(:all)',
-      'action'  => function ($slug) {
+        // Get the first set of pages to display primary nav
+        'pattern' => 'mgfagency/helpsection/page/(:all)',
+        'action'  => function ($slug) {
         return [
-          'rendered' => page($slug)->render(),
+            'rendered' => page($slug)->render(),
         ];
-      }
+        }
     ],
-  ],
+    ],
 ];
--- index.php
+++ index.php
@@ -4,19 +4,20 @@ namespace mgfagency\helpsection;
 
 use Kirby;
 
+
 \Kirby::plugin('mgfagency/helpsection', [
-  'routes' => function ($kirby) {
-    return [
-      [
-        'pattern' => $kirby->option('mgfagency.helpsection.contentfolder', 'helpsection'),
-        'action' => function () {
-          return false;
-        }
-      ]
-    ];
-  },
-  'api' => include __DIR__ . '/includes/api.php',
-  'templates' => [
-    'doc' => __DIR__ . '/templates/doc.php',
-  ],
+    'routes' => function ($kirby) {
+        return [
+            [
+                'pattern' => $kirby->option('mgfagency.helpsection.contentfolder', 'helpsection'),
+                'action' => function () {
+                    return false;
+                }
+            ]
+        ];
+    },
+    'api' => include __DIR__ . '/includes/api.php',
+    'templates' => [
+        'doc' => __DIR__ . '/templates/doc.php',
+    ],
 ]);
